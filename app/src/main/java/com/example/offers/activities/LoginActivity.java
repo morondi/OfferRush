85f1c5b400673bd4d2b49688938a010639f89591
morondi <morondibrian67@gmail.com>
2023-06-25 14:36:03 +0300

    login

---
+++ app/src/main/java/com/example/offers/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.offers.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.offers.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/offers/activities/PaymentActivity.java
@@ -0,0 +1,35 @@
+package com.example.offers.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.offers.R;
+import com.example.offers.databinding.ActivityPaymentBinding;
+import com.example.offers.utils.Constants;
+
+public class PaymentActivity extends AppCompatActivity {
+
+    ActivityPaymentBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityPaymentBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        String orderCode = getIntent().getStringExtra("orderCode");
+
+        binding.webview.setMixedContentAllowed(true);
+        binding.webview.loadUrl(Constants.PAYMENT_URL + orderCode);
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return super.onSupportNavigateUp();
+    }
+}
